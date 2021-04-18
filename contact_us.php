572377be4b3621a6026d6969ecb1e63d60eae9ce
manishzalte <zaltemanish@gmail.com>
2021-04-18 13:02:39 +0530

    first commit

---
+++ contact_us.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Contact Us</title>
+<link rel="stylesheet" href="css/contact_us.css" />
+
+<?php
+include_once"setting/contactus_navigation.php";
+?>
+</head>
+
+<body>
+<div id="contact">
+<p class="p1">Contact</p>
+<p class="p2">Department Of Computer Science </p>
+<p class="p3">SSBT COET<br>
+Bambhori<br>
+Jalagon - 425001<br>
+Maharashtra (MH)<br><br></p>
+<table>
+<tr>
+	<td>
+			<p class="p2">Contact us at:</p>
+			<p class="p3">Tel : +XXXX XXX XXx</p>
+			<p class="p3">Fax: +XXX XXX XXX </p>
+	</td>		
+	<td >
+    <p class="p2">Email:</p>emailus@ssbtcoet.ac.in<p class="p3"></p><br /><br />
+    <p class="p2">Our Social Pages:</p>
+  <p class="p3"> <a href="https://www.facebook.com/">
+   <img src="pictures/facebook.png" width="40" height="40"></a> 
+   <a href="https://www.instagram.com/"> 
+   <img src="pictures/instagram.png" width="55" height="40">
+  </a> <a href="https://twitter.com/">
+  <img src="pictures/twitter.png" width="40" height="40"></a></p><br /><br />
+    </td>
+</tr>
+</table>
+
+</div>
+<br><br><br>
+<br /><hr color="#050119" size="4"/>
+<br>
+<div id="faq">
+<p class="p1">FAQ</p>
+<p class="p2"><br>Who are our Alumni?</p>
+<p class="p3">All individuals who have followed Diploma, Undergraduate, Postgraduate or PhD of 
+the programmes of SSBT COET can register as our alumni.</p>
+<p class="p2"><br>How do I become a member of the SSBT COET Alumni?</p>
+<p class="p3">All undergraduates and post graduates of SSBT COET can register as
+ our alumni. It is important for alumni to update their particulars for the SSBT COET Alumni
+  Directory (Database) by register as a member online and pay at the counter of Alumni 
+  SSBT COET so that we can keep in touch with you for life.</p>
+<p class="p2"><br>Do I need to pay any fee?</p>
+
+<p class="p3">Yes, but it is only for membership registration. You can choose to pay
+ yearly or pay for Life Membership. The fee for yearly paid and life membership are Rs 1000
+  and Rs 2000 respectively. </p>
+<p class="p2"><br>As an alumni, how can I contribute towards the University?</p>
+<p class="p3">You can help by participating in the various activities organized by 
+the University or by contributing ideas or programmes towards enriching learning 
+experience of all students. Other assistance include donation, sponsorship, expertise
+ and volunteer services. Whatever your contribution they are most welcome.</p>
+<p class="p2"><br>I am still a student of the University, can I take part in alumni 
+activities?</p>
+<p class="p3">Yes, of course. We believed that the bond between alumni and students
+ should begin as soon as the student enters the University because you will become
+  our future alumni later on. The Alumni Advancement is also committed in organising 
+  training especially in soft skills and career development programmes to raise your
+   employability in the job market upon graduation. We hope this will instill a pride 
+   in your alma mater and later on may wish to make whatever contribution towards 
+   achieving excellence for the University.
+</p>
+</div>
+<br><br><br><br>
+</body>
+</html>
